209a72027f691da3e551f36b8ee7059ce0a086c3
marykamau2 <mary.kamau@student.moringaschool.com>
2021-09-06 20:12:37 +0300

    added the owner model

--- mypics/models.py
+++ mypics/models.py
@@ -1,23 +1,23 @@
 
-# from django.db import models
-# import datetime as dt
-# from cloudinary.models import CloudinaryField
+from django.db import models
+import datetime as dt
+from cloudinary.models import CloudinaryField
 
-# # Create your models here.
-# class Owner(models.Model):
-#   first_name = models.CharField(max_length=30)
-#   sur_name = models.CharField(max_length=30)
-#   email = models.EmailField()
-#   phone_number = models.CharField(max_length=10,blank=True)
+# Create your models here.
+class Owner(models.Model):
+  first_name = models.CharField(max_length=30)
+  sur_name = models.CharField(max_length=30)
+  email = models.EmailField()
+  phone_number = models.CharField(max_length=10,blank=True)
 
-#   def __str__(self):
-#       return self.first_name
+  def __str__(self):
+      return self.first_name
 
-#   class meta:
-#     ordering = ['name']
+  class meta:
+    ordering = ['name']
 
-#   def save_owner(self):
-#     self.save()
+  def save_owner(self):
+    self.save()
 
 # class Location(models.Model):
 #   title = models.CharField(max_length =50)
