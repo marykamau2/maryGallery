5807025a27a94f4a881653351ae78ff6ffee4782
marykamau2 <mary.kamau@student.moringaschool.com>
2021-09-06 21:24:01 +0300

    defined the app

--- mypics/apps.py
+++ mypics/apps.py
@@ -1,5 +1,5 @@
-# from django.apps import AppConfig
+from django.apps import AppConfig
 
 
-# class GalleryConfig(AppConfig):
-#     name = 'mypics'
+class GalleryConfig(AppConfig):
+    name = 'mypics'
