c86b056ca06c824c80da4dfef0455284b8439a0b
marykamau2 <mary.kamau@student.moringaschool.com>
2021-09-06 20:16:16 +0300

    added the search  model

--- mypics/models.py
+++ mypics/models.py
@@ -70,37 +70,37 @@ class Photo(models.Model):
   location = models.ForeignKey(Location,on_delete=models.CASCADE)
   category = models.ForeignKey(Category,on_delete=models.CASCADE)
 
-  # def save_photo(self):
-  #   self.save()
+  def save_photo(self):
+    self.save()
 
-  # def delete_photo(self):
-  #   self.delete()
+  def delete_photo(self):
+    self.delete()
 
-  # @classmethod
-  # def update_photo(cls, id ,title,description ,owner, location, category):
-  #   update = cls.objects.filter(id = id).update(title = title,description = description,owner=owner,location = location,category = category)
-  #   return update
+  @classmethod
+  def update_photo(cls, id ,title,description ,owner, location, category):
+    update = cls.objects.filter(id = id).update(title = title,description = description,owner=owner,location = location,category = category)
+    return update
 
-  # @classmethod
-  # def get_all_photos(cls):
-  #   photos = cls.objects.all()
-  #   return photos
+  @classmethod
+  def get_all_photos(cls):
+    photos = cls.objects.all()
+    return photos
 
-  # @classmethod
-  # def get_photo_id(cls,id):
-  #   photo_id = cls.objects.filter(id= id).all()
-  #   return photo_id
+  @classmethod
+  def get_photo_id(cls,id):
+    photo_id = cls.objects.filter(id= id).all()
+    return photo_id
 
-  # @classmethod
-  # def filter_photo_by_location(cls,location):
-  #   photos = Photo.objects.filter(location__title__icontains=location)
-  #   return location
+  @classmethod
+  def filter_photo_by_location(cls,location):
+    photos = Photo.objects.filter(location__title__icontains=location)
+    return location
 
-  # @classmethod
-  # def search_photos_by_category(cls,category):
-  #     photos = Photo.objects.filter(category__title__icontains=category)
-  #     return photos
+  @classmethod
+  def search_photos_by_category(cls,category):
+      photos = Photo.objects.filter(category__title__icontains=category)
+      return photos
 
 
-  # def __str__(self):
-  #   return self.title
+  def __str__(self):
+    return self.title
